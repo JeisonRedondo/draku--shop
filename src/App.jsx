973513cbfa6223a8fc19234827ed242b220c86e8
JeisonR <jeida16@yahoo.es>
2023-10-13 19:46:46 -0500

    [ADD]Inicio del proyecto, archivos base y configuración para uso de tailwind y vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        Hola parce
+      </div>
+    </>
+  )
+}
+
+export default App
