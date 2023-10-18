2ed7a6bbe99aff2b9b0ca1f5a40f946e9ed0c03b
JeisonR <jeida16@yahoo.es>
2023-10-18 18:43:31 -0500

    [ADD] Createcontext y agregado en App

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,18 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from '../../Routes';
 import { Navbar } from '../../Components/Navbar';
+import { ShoppingCartProvider } from '../../Context';
 
 import './App.css';
 import { Layout } from '../../Components/Layout';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Layout >
-        <AppRoutes />
-      </Layout>
-    </BrowserRouter>
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Layout >
+          <AppRoutes />
+        </Layout>
+      </BrowserRouter>
+    </ShoppingCartProvider>
   )
 }
 
