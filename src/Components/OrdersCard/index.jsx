7d361bbe0bb83c7324582b7c216e8e9fb00ba0ff
JeisonR <jeida16@yahoo.es>
2023-11-01 20:12:01 -0500

    [CHANGE]Cambios hechos al estilo de presentación de MyOrders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,4 +1,3 @@
-import { useShoppingCartContext } from "../../Context";
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 import { CalendarDaysIcon } from "@heroicons/react/24/solid";
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid";
@@ -8,7 +7,8 @@ export function OrdersCard(props) {
 
   const { totalPrice, totalProducts, date } = props;
   return (
-    <div className="flex justify-between items-center mb-3 border border-black p-2 rounded">
+    <div className="flex justify-between gap-2 items-center mb-3 
+      border border-black p-2 rounded">
       <div className="flex flex-col">
         <p className="flex flex-row gap-2">
           <CalendarDaysIcon className="w-5 h-6 text-black" />
@@ -18,13 +18,12 @@ export function OrdersCard(props) {
           <ShoppingCartIcon className="w-5 h-6 text-black" />
           <span> {totalProducts} products</span>
         </p>
-        <p className="flex flex-row gap-2">
-          <CurrencyDollarIcon className="w-5 h-6 text-black" />
-          <span>{totalPrice}</span>
-        </p>
-
 
       </div>
+      <p className="flex flex-row text-2xl">
+        <CurrencyDollarIcon className="w-5 h-6 text-black" />
+        <span>{totalPrice}</span>
+      </p>
     </div>
   )
 };
